d001d7d987febb1d263fb9feff0b2491f0d148a6
Shekhina01 <54628737+Shekhina01@users.noreply.github.com>
2021-06-06 13:24:44 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      
+      <h1>Hi I made this project during the 7 Days Free Bootcamp, conducted by <b> SHAPEAI</b>.</h1>
+      <p>The instructor during the session was Mr. Shaurya Sinha (Data Analyst Intern at Jio). I got to
+learn a lot during these 7 days and it was an amazing experience learning with SHAPEAI. 
+<br></br>Here's the link for you to watch the sessions as well:
+
+<a href="https://www.youtube.com/playlist?list=PL7zl8TDRnbulLetcbkthT0p_IzwgRAYbu"> Click here </a>
+<img src="https://raw.githubusercontent.com/ShapeAI/PYTHON-AND-DATA-ANALYTICS/main/YOUTUBE%20THUMBNAIL-4.png" width="900" height="700"/>
+<br></br>
+</p>
+<p><b>I got to have hands on experience on:
+</b></p>
+<p>
+<ul>
+  <li>JAVASCRIPT</li>
+  <li>HTML</li>
+  <li>REACTJS</li>
+</ul> 
+</p>
+<p>During these 7 days, and everything was explained from the very basics so that
+anyone with zero experience on programming can learn.
+I enjoyed these 7 days, you can as well. To register for next free 7 days bootcamp, visit:
+<a href="https://www.shapeai.tech"> www.shapeai.tech </a> 
+or follow SHAPEAI on:
+</p>
+
+<ul>
+  <li><a href=
+"https://in.linkedin.com/company/shapeai">LinkedIn</a>
+</li>
+  <li><a href=
+"https://www.instagram.com/shape.ai/?hl=en">Instagram</a>
+</li>
+  <li><a
+href=
+"https://www.youtube.com/channel/UCTUvDLTW9meuDXWcbmISPdA">YouTube</a>
+</li>
+<li><a href="https://github.com/shapeai">GitHub</a>
+</li>
+</ul> 
+
+</div>
+
+    
+  );
+}
+export default Note;
